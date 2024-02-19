c0dcd4ef984d2c8061b86b61818c75f0026277b5
simona <s.nasteva22@gmail.com>
2024-02-19 10:51:54 +0200

    Improve room searcg

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,9 +1,11 @@
+import PageSearch from "@/components/PageSearch/PageSearch";
 import Hero from "@/components/hero/hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <PageSearch />
       {/* search */}
     </>
   );
