dd58f718bbc7f9d433923df65b943ee62ac1a54c
simona <s.nasteva22@gmail.com>
2024-03-16 10:38:35 +0200

    Add page api

--- libs/api.ts
+++ libs/api.ts
@@ -14,6 +14,6 @@ export async function getFeaturedRoom() {
 }
 
 export async function getRooms() {
-  const result = await sanityCLient.fetch(queries.getRoom);
+  const result = await sanityCLient.fetch(queries.getRoomsQuery);
   return result;
 }
